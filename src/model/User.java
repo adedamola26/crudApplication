be64b5cb0a128ecc1def2a505ae642216d98e53a
adedamola26 <adesoye.a@northeastern.edu>
2023-02-19 09:44:09 -0500

    half done model

---
+++ src/model/User.java
@@ -0,0 +1,98 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import javax.swing.Icon;
+
+/**
+ *
+ * @author adeda
+ */
+public class User {
+    
+    String name;
+    String id;
+    String age;
+    String gender;
+    String date;
+    String level;
+    String cellNum;
+    String email;
+    Icon photo;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getCellNum() {
+        return cellNum;
+    }
+
+    public void setCellNum(String cellNum) {
+        this.cellNum = cellNum;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Icon getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Icon photo) {
+        this.photo = photo;
+    }
+    
+
+}
